cff584d843594c0fbeb3a924934df0e67b2901b8
Paul Mattingly <paul.mattingly@niu-solutions.com>
2018-12-28 17:04:31 +0000

    First add

---
+++ Flags/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Flags.Startup))]
+namespace Flags
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
