e78b4b69c33b286022ee48321226e77b03f296aa
Paul Mattingly <paul.mattingly@niu-solutions.com>
2019-03-04 18:51:36 +0000

    Adding files

---
+++ Models/Quiz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Question
+    {
+        public Guid ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public IEnumerable<Flag> FlagChoices { get; set; }
+        public Guid AspNetUserID { get; set; }
+        public Guid UserAnswer { get; set; }
+        public Guid CorrectAnswer { get; set; }
+        
+
+    }
+
+
+ 
+}
