bf675fb705e0bf79e6ff1a5b9fac119c633488ae
Paul Mattingly <paul.mattingly@niu-solutions.com>
2019-05-19 00:29:46 +0100

    Added these

---
+++ Flags/Models/QuestionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Flags.Models
+{
+    public class QuestionViewModel
+    {
+        public Guid ID { get; set; }
+        public IEnumerable<Flag> FlagChoices { get; set; }
+        public Guid UserAnswer { get; set; }
+    }
+}
